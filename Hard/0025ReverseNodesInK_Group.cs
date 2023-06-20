baadb62664d94b1aa8a59a4d901cd632e05e32c3
brto <64071930+Go-Ba@users.noreply.github.com>
2023-06-20 16:43:50 +1000

    Hard + Daily

---
+++ Hard/0025ReverseNodesInK_Group.cs
@@ -0,0 +1,56 @@
+﻿public class ReverseNodesInK_Group
+{
+    //https://leetcode.com/problems/reverse-nodes-in-k-group/
+    public class Solution
+    {
+        public ListNode ReverseKGroup(ListNode head, int k)
+        {
+            ListNode dummyHead = new(-1, head);
+            ListNode previousListEnd = dummyHead;
+            ListNode nextListStart;
+
+            while(true)
+            {
+                var kth = GetK(previousListEnd, k);
+                if (kth == null)
+                    break;
+
+                nextListStart = kth.next;
+                ListNode current = previousListEnd.next;
+                ListNode previous = nextListStart;
+
+                while (current != nextListStart)
+                {
+                    var next = current.next;
+                    current.next = previous;
+                    previous = current;
+                    current = next;
+                }
+                var listStart = previousListEnd.next;
+                previousListEnd.next = kth;
+                previousListEnd = listStart;
+            }
+            return dummyHead.next;
+        }
+        ListNode GetK(ListNode node, int k)
+        {
+            ListNode current = node;
+            while(current != null && k > 0)
+            {
+                current = current.next;
+                k--;
+            }
+            return current;
+        }
+    }
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
---
+++ Medium/2090KRadiusSubarrayAverages.cs
@@ -0,0 +1,63 @@
+﻿public class KRadiusSubarrayAverages
+{
+    //https://leetcode.com/problems/k-radius-subarray-averages/
+    public class Solution_WindowSum_2
+    {
+        public int[] GetAverages(int[] nums, int k)
+        {
+            long sum = 0;
+            int[] averages = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                //null out the edges
+                if (i < k || i >= nums.Length - k)
+                {
+                    averages[i] = -1;
+                    continue;
+                }
+                //if previous is empty, calculate full sum
+                else if (i - k == 0 || nums[i - 1] == -1)
+                    for (int j = i - k; j <= i + k; j++)
+                        sum += nums[j];
+                //else, just modify the two ends of the previous sum to make it your own
+                else sum = sum - nums[i - k - 1] + nums[i + k];
+                //calculate average
+                averages[i] = (int)(sum / (k * 2 + 1));
+            }
+            return averages;
+        }
+    }
+    public class Solution_WindowSum
+    {
+        public int[] GetAverages(int[] nums, int k)
+        {
+            long[] sums = new long[nums.Length];
+            int[] averages = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                //null out the edges
+                if (i < k || i >= nums.Length - k)
+                    sums[i] = -1;
+                //if previous is empty, get full sum
+                else if (i - k == 0 || nums[i - 1] == -1)
+                    for (int j = i - k; j <= i + k; j++)
+                        sums[i] += nums[j];
+                //else, just modify the two ends of the previous sum to make it your own
+                else
+                    sums[i] = sums[i - 1] - nums[i - k - 1] + nums[i + k];
+            }
+            //turn sum into average
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (sums[i] != -1)
+                    averages[i] = (int)(sums[i] / (k * 2 + 1));
+                else
+                    averages[i] = -1;
+            }
+            
+            return averages;
+        }
+    }
+}
