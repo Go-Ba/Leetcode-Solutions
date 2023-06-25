bf98e0c4d916f997294879c7d5b1f67f28fa52ea
brto <64071930+Go-Ba@users.noreply.github.com>
2023-06-25 14:03:41 +1000

    Sliding Window

---
+++ Medium/0003LongestSubstringWithoutRepeatingCharaters.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class LongestSubstringWithoutRepeatingCharaters
+{
+    //https://leetcode.com/problems/longest-substring-without-repeating-characters/
+    public class Solution
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            if (s.Length <= 1)
+                return s.Length;
+
+            int l = 0, r = 0, longest = 1;
+            HashSet<char> existingChars = new();
+            while (r < s.Length)
+            {
+                if (l != r && existingChars.Contains(s[r]))
+                {
+                    existingChars.Remove(s[l]);
+                    l++;
+                }
+                else
+                {
+                    existingChars.Add(s[r]);
+                    longest = Math.Max(longest, r - l + 1);
+                    r++;
+                }
+            }
+            return longest;
+        }
+    }
+}
---
+++ Medium/0424LongestRepeatingCharacterReplacement.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class LongestRepeatingCharacterReplacement
+{
+    //https://leetcode.com/problems/longest-repeating-character-replacement/
+    public class Solution
+    {
+        public int CharacterReplacement(string s, int k)
+        {
+            Dictionary<int, int> map = new();
+            int left = 0;
+            int longest = 0;
+            int highestFrequency = 0;
+            for (int right = 0; right < s.Length; right++)
+            {
+                //add current right to dictionary
+                if (!map.ContainsKey(s[right]))               
+                    map.Add(s[right], 1);                
+                else               
+                    map[s[right]]++;
+                
+                //check if the frequency of rightmost char is more than current highest
+                highestFrequency = Math.Max(map[s[right]], highestFrequency);
+
+                //after removing the count of the highest frequency char
+                //check if the remainder is less than max replacements
+                //if not, move left pointer forward until it is
+                while ((right - left + 1) - highestFrequency > k)
+                {
+                    map[s[left]]--;
+                    left++;
+                }
+
+                //keep track of longest string
+                longest = Math.Max(right - left + 1, longest);
+            }
+            return longest;
+        }
+    }
+}
