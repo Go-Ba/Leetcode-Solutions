456d7ff61f4ea7380ac11a6bf2ce8195b1f3b50f
brto <64071930+Go-Ba@users.noreply.github.com>
2023-07-01 17:06:21 +1000

    stack questions

---
+++ Medium/0022GenerateParentheses.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+
+public class GenerateParentheses
+{
+    //https://leetcode.com/problems/generate-parentheses/
+    public class Solution2
+    {
+        List<string> validStrings;
+        int maxPairs;
+        public IList<string> GenerateParenthesis(int n)
+        {
+            validStrings = new List<string>();
+            maxPairs = n;
+            GetStringList("(", 1, 0);
+            return validStrings;
+        }
+        public void GetStringList(string currentString, int left, int right)
+        {
+            if (left + right == maxPairs * 2)
+            {
+                if (currentString[^1] == ')')
+                    validStrings.Add(currentString);
+                return;
+            }
+
+            if (left < maxPairs)
+                GetStringList(currentString + "(", left + 1, right);
+            if (right < maxPairs && left > right)
+                GetStringList(currentString + ")", left, right + 1);
+        }
+    }
+    public class Solution1
+    {
+        HashSet<string> validStrings;
+        public IList<string> GenerateParenthesis(int n)
+        {
+            validStrings = new HashSet<string>();
+            GetStringList("(", 1, 0, n);
+            List<string> output = new List<string>();
+            foreach (var item in validStrings)            
+                output.Add(item);
+            return output;
+        }
+        public void GetStringList(string currentString, int left, int right, int maxPairs)
+        {
+            if (left + right == maxPairs * 2)
+            {
+                if (currentString[^1] == ')')
+                    validStrings.Add(currentString);
+                return;
+            }
+
+            if (left < maxPairs)
+                GetStringList(currentString + "(", left + 1, right, maxPairs);
+            if (right < maxPairs && left > right)
+                GetStringList(currentString + ")", left, right + 1, maxPairs);
+        }
+    }
+}
---
+++ Medium/0739DailyTemperatures.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+public class DailyTemperatures
+{
+    //https://leetcode.com/problems/daily-temperatures/
+    public class Solution_Stack
+    {
+        public int[] DailyTemperatures(int[] temperatures)
+        {
+            int[] output = new int[temperatures.Length];
+            Stack<int> indexStack = new();
+            indexStack.Push(0);
+            for (int i = 1; i < temperatures.Length; i++)
+            {
+                while (indexStack.Count > 0 && temperatures[indexStack.Peek()] < temperatures[i])
+                {
+                    int index = indexStack.Pop();
+                    output[index] = i - index;
+                }
+                indexStack.Push(i);
+            }
+            return output;
+        }
+    }
+    public class Solution_Naive
+    {
+        public int[] DailyTemperatures(int[] temperatures)
+        {
+            int[] output = new int[temperatures.Length];
+            for (int i = 0; i < temperatures.Length; i++)
+                for (int j = i + 1; j < temperatures.Length; j++)
+                    if (temperatures[j] > temperatures[i])
+                    {
+                        output[i] = j - i;
+                        break;
+                    }
+             return output;       
+        }
+    }
+}
