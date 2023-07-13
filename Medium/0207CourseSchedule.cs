8e92301d3efafc056515cd28673a105608675351
brto <64071930+Go-Ba@users.noreply.github.com>
2023-07-13 10:59:14 +1000

    Dailies

---
+++ Medium/0207CourseSchedule.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+public class CourseSchedule
+{
+    //https://leetcode.com/problems/course-schedule/
+    public class Solution
+    {
+        public bool CanFinish(int numCourses, int[][] prerequisites)
+        {
+            List<int>[] graph = new List<int>[numCourses];
+            foreach (var prerequisite in prerequisites)
+            {
+                if (graph[prerequisite[0]] == null)
+                    graph[prerequisite[0]] = new();
+                graph[prerequisite[0]].Add(prerequisite[1]);
+            }
+
+            //copied from problem 802
+            HashSet<int> traversed = new();
+            bool?[] isSafe = new bool?[graph.Length];
+
+            for (int i = 0; i < graph.Length; i++)
+                if (!IsSafeNode(i))
+                    return false;
+            return true;
+
+            bool IsSafeNode(int current)
+            {
+                if (isSafe[current] != null)
+                    return isSafe[current] == true;
+
+                traversed.Add(current);
+                bool safeNeighbours = AreNeighboursSafe(graph[current]);
+                traversed.Remove(current);
+
+                isSafe[current] = safeNeighbours;
+                return safeNeighbours;
+            }
+            bool AreNeighboursSafe(List<int> neighbours)
+            {
+                if (neighbours == null)
+                    return true;
+                foreach (int neighbour in neighbours)
+                    if (traversed.Contains(neighbour) || !IsSafeNode(neighbour))
+                        return false;
+                return true;
+            }
+        }
+    }
+}
---
+++ Medium/0802FindEventualSafeStates.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+
+public class FindEventualSafeStates
+{
+    //https://leetcode.com/problems/find-eventual-safe-states/
+    public class Solution2
+    {
+        public IList<int> EventualSafeNodes(int[][] graph)
+        {
+            List<int> safeNodes = new();
+            HashSet<int> traversed = new();
+            bool?[] isSafe = new bool?[graph.Length];
+
+            for (int i = 0; i < graph.Length; i++)
+                if (IsSafeNode(i))
+                    safeNodes.Add(i);
+
+            return safeNodes;
+
+            bool IsSafeNode(int current)
+            {
+                if (isSafe[current] != null)
+                    return isSafe[current] == true;
+
+                traversed.Add(current);
+                bool safeNeighbours = AreNeighboursSafe(graph[current]);
+                traversed.Remove(current);
+
+                isSafe[current] = safeNeighbours;
+                return safeNeighbours;
+            }
+            bool AreNeighboursSafe(int[] neighbours)
+            {
+                foreach (int neighbour in neighbours)
+                    if (traversed.Contains(neighbour) || !IsSafeNode(neighbour))
+                        return false;
+                return true;
+            }
+        }
+    }
+    public class Solution1
+    {
+        bool?[] isSafe;
+        int[][] graph;
+        HashSet<int> traversed;
+        public IList<int> EventualSafeNodes(int[][] graph)
+        {
+            isSafe = new bool?[graph.Length];
+            this.graph = graph;
+            List<int> safeNodes = new List<int>();
+            traversed = new HashSet<int>();
+
+            for (int i = 0; i < graph.Length; i++)
+            {
+                if (IsSafeNode(i))
+                {
+                    safeNodes.Add(i);
+                }
+            }
+
+            return safeNodes;
+        }
+        bool IsSafeNode(int current)
+        {
+            if (isSafe[current] != null)
+                return isSafe[current] == true;
+
+            traversed.Add(current);
+            var neighbours = graph[current];
+
+            for (int i = 0; i < neighbours.Length; i++)
+            {            
+                if (traversed.Contains(neighbours[i]) || !IsSafeNode(neighbours[i]))
+                {
+                    traversed.Remove(current);
+                    isSafe[current] = false;
+                    return false;
+                }
+            }
+            traversed.Remove(current);
+            isSafe[current] = true;
+            return true;
+        }
+    }
+}
