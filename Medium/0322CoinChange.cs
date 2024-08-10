057bd725b77a7038828051d1de6c90dc6609470e
brto <64071930+Go-Ba@users.noreply.github.com>
2024-08-10 14:14:27 +1000

    Coin Change

---
+++ Medium/0322CoinChange.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class CoinChange
+{
+    public class Solution1
+    {
+        int minCount;
+        public int CoinChange(int[] coins, int amount)
+        {
+            if (amount == 0) return 0;
+            minCount = -1;
+            GetCombinationCount(0, 0, 0, coins, amount);
+            return minCount;
+        }
+        public void GetCombinationCount(int nextCoin, int currentTotal, int currentCount, int[] coins, int goalTotal)
+        {
+            if (currentCount >= minCount && minCount > 0)
+                return;
+            currentTotal += nextCoin;
+            if (currentTotal > goalTotal)
+                return;
+            if (currentTotal == goalTotal)
+            {
+                minCount = currentCount;
+                return;
+            }
+            foreach (var coin in coins)
+            {
+                if (coin < nextCoin) continue;
+                GetCombinationCount(coin, currentTotal, currentCount + 1, coins, goalTotal);
+            }
+        }
+    }
+    public class Solution2
+    {
+        Dictionary<(int, long), int> CoinAndTotalToDistance;
+        public int CoinChange(int[] coins, int amount)
+        {
+            if (amount == 0) return 0;
+            CoinAndTotalToDistance = new();
+            return GetDistanceToGoal(0, 0, coins, amount);
+        }
+        public int GetDistanceToGoal(int nextCoin, long currentTotal, int[] coins, int goalTotal)
+        {
+            if (CoinAndTotalToDistance.TryGetValue((nextCoin, currentTotal), out int result))
+                return result;
+
+            var newTotal = currentTotal + nextCoin;
+            if (newTotal > goalTotal)
+                return -1;
+            if (newTotal == goalTotal)
+            {
+                CoinAndTotalToDistance.Add((nextCoin, currentTotal), 0);
+                return 0;
+            }
+            var minDis = -1;
+            foreach (var coin in coins)
+            {
+                var dis = GetDistanceToGoal(coin, newTotal, coins, goalTotal);
+                if (minDis < 0) minDis = dis;
+                if (dis < minDis && dis >= 0) minDis = dis;
+            }
+            if (minDis >= 0)
+                minDis++;
+            CoinAndTotalToDistance.Add((nextCoin, currentTotal), minDis);
+            return minDis;
+        }
+    }
+    public class Solution
+    {
+        int[] cache;
+        public int CoinChange(int[] coins, int amount)
+        {
+            cache = new int[amount + 1];
+            for (int i = 0; i < amount + 1; i++)           
+                cache[i] = amount + 1;
+            cache[0] = 0;
+
+            foreach (int coin in coins)            
+                for (int i = coin; i <= amount; i++)               
+                    cache[i] = Math.Min(cache[i], cache[i - coin] + 1);
+
+            return cache[amount] <= amount ? cache[amount] : -1;
+        }
+    }
+
+    /*
+
+    1,2,5
+    6
+    0,1,1,2,2,1,2
+    
+    */
+
+}
---
+++ Medium/0518CoinChangeII.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+public class CoinChangeII
+{
+    public class Solution1
+    {
+        Dictionary<(int, int), int> cache;
+        public int Change(int amount, int[] coins)
+        {
+            if (amount == 0) return 1;
+            cache = new();
+            int solutions = 0;
+            foreach (var coin in coins)
+                solutions += GetCombinationCount(coin, 0, coins, amount);
+            return solutions;
+        }
+        public int GetCombinationCount(int nextCoin, int currentTotal, int[] coins, int goalTotal)
+        {
+            if (cache.TryGetValue((nextCoin, currentTotal), out int result))
+                return result;
+            int newTotal = currentTotal + nextCoin;
+
+            if (newTotal > goalTotal)
+            {
+                cache.Add((nextCoin, currentTotal), 0);
+                return 0;
+            }
+            if (newTotal == goalTotal)
+            {
+                cache.Add((nextCoin, currentTotal), 1);
+                return 1;
+            }
+
+            int solutions = 0;
+            foreach (var coin in coins)
+            {
+                if (coin > nextCoin) continue;
+                solutions += GetCombinationCount(coin, newTotal, coins, goalTotal);
+            }
+            cache.Add((nextCoin, currentTotal), solutions);
+            return solutions;
+        }
+    }
+}
